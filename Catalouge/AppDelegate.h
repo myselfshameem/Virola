a9ad4c648ec710cc7671e8c9b5c3f15adb4f402c
Shameem Ahamad <shameem.ahamad@citixsys.com>
2015-04-13 21:25:14 +0530

    Initial Commit

---
+++ Catalouge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Catalouge
+//
+//  Created by Shameem Ahamad on 4/13/15.
+//  Copyright (c) 2015 Shameem Ahamad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Catalouge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Catalouge
+//
+//  Created by Shameem Ahamad on 4/13/15.
+//  Copyright (c) 2015 Shameem Ahamad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
